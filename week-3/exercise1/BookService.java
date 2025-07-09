9f1d18ae690d6506aaabf7b4417f035bdbb98319
veeresh <149483766+veereshkummara@users.noreply.github.com>
2025-07-09 17:25:34 +0530

    Create BookService.java

---
+++ week-3/exercise1/BookService.java
@@ -0,0 +1,18 @@
+package com.library.service;
+
+import com.library.repository.BookRepository;
+
+public class BookService {
+
+    private BookRepository bookRepository;
+
+    // Setter for dependency injection
+    public void setBookRepository(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public void addBook(String title) {
+        bookRepository.saveBook(title);
+        System.out.println("BookService: Book added successfully.");
+    }
+}
