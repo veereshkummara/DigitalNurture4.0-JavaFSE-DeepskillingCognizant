9753d67ca20d72de81a641df394f904cf9210245
veeresh <149483766+veereshkummara@users.noreply.github.com>
2025-07-30 16:32:16 +0530

    Create App.js

---
+++ week6/react-hol-2/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Welcome from './Welcome';
+
+function App() {
+  return (
+    <div>
+      <Welcome name="John" />
+      <Welcome name="Jane" />
+      <Welcome name="Alice" />
+    </div>
+  );
+}
+
+export default App;
