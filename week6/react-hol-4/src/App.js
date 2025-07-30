e3f5f661686353e74c27922b2f5fae8b128cb7ba
veeresh <149483766+veereshkummara@users.noreply.github.com>
2025-07-30 16:34:51 +0530

    Create App.js

---
+++ week6/react-hol-4/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ClickHandler from './ClickHandler';
+
+function App() {
+  return (
+    <div>
+      <ClickHandler />
+    </div>
+  );
+}
+
+export default App;
