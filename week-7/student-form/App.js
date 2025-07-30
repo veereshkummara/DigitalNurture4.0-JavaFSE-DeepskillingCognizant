caad2ec73f0732775a78d1d835c7d24e1f316bb5
veeresh <149483766+veereshkummara@users.noreply.github.com>
2025-07-30 16:41:46 +0530

    Create App.js

---
+++ week-7/student-form/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import StudentForm from './StudentForm';
+
+function App() {
+  return (
+    <div className="App">
+      <StudentForm />
+    </div>
+  );
+}
+
+export default App;
